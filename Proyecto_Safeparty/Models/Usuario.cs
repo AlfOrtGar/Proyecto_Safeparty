aa14cc186208533d8a0661d1d5e7b55d57945638
alfortgar <alfortprofesional@gmail.com>
2023-12-15 11:58:42 +0100

    Implementacion de sesion y filtro

--- Proyecto_Safeparty/Models/Usuario.cs
+++ Proyecto_Safeparty/Models/Usuario.cs
@@ -7,7 +7,7 @@
         public string apellidos { get; set; }
         public DateTime fecha_nacimiento { get; set; }
         public string genero { get; set; }
-        public string telefono { get; set; }
+        public string password { get; set; }
         public string username { get; set; }
         public string email { get; set; }
 
--- Proyecto_Safeparty/Program.cs
+++ Proyecto_Safeparty/Program.cs
@@ -1,10 +1,26 @@
+using Proyecto_Safeparty.Permissions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+builder.Services.AddScoped<ValidacionLoginAttribute>();
+builder.Services.AddMvc(options =>
+{
+    options.Filters.AddService<ValidacionLoginAttribute>();
+});
+
 var app = builder.Build();
 
+
+app.UseSession();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -22,6 +38,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Acceso}/{action=Login}/{id?}");
 
 app.Run();
